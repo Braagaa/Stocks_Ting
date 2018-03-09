cd208830c9427c0fd8dc90c7c30f58e4af6e3274
Michael Braga <michael_braga@outlook.com>
2018-03-08 22:47:51 -0500

    able to properly round properties

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const R = require('ramda');
 const {join, resolve} = require('path');
 const express = require('express');
 const ejs = require('ejs');
+const roundTo = R.curry(require('round-to'));
 
 const app = express();
 
@@ -25,10 +26,26 @@ const dividenPred = R.pipe(
 );
 const payoutRatioPred = R.pipe(R.prop('AE'), R.lte(R.__, 100));
 
+const roundProp = R.curry((prop, round, obj) => R.pipe(
+    R.prop(prop), 
+    R.tryCatch(roundTo(R.__, round), R.always('n/a'))
+)(obj));
+
 const preds = {
     yieldPred,
     dividenPred,
     payoutRatioPred
+};
+
+const renamedProps = {
+    Ticker: R.prop('B'),
+    Company: R.prop('C'),
+    Yield: roundProp('F', 2),
+    '1-yr': roundProp('I', 1),
+    '3-yr': roundProp('J', 1),
+    '5-yr': roundProp('K', 1),
+    '10-yr': roundProp('L', 1),
+    'TTM EPS': roundProp('AE', 2)
 };
 
 const filterStocks = R.pipe(
@@ -39,7 +56,6 @@ const filterStocks = R.pipe(
     R.gte(2)
 );
 
-
 const v1 = R.pipe(
     R.prop('Canadian Dividend All-Star List'),
     R.filter(R.propIs(Number, 'A')),
@@ -47,14 +63,16 @@ const v1 = R.pipe(
     R.map(R.pick(['B', 'C', 'F', 'I', 'J', 'K', 'L', 'AE']))
 );
 
-const wantedTitle = R.pipe();
-
-app.use((req, res) => {
-
-});
+const wantedTitles = ['B', 'C', 'F', 'I', 'J', 'K', 'L', 'AE'];
+const selectedTitle = R.pipe(
+    R.prop('Canadian Dividend All-Star List'),
+    R.filter(R.propIs(Number, 'A')),
+    R.map(R.pick(wantedTitles)),
+    R.map(R.applySpec(renamedProps))
+);
 
 app.get('/', (req, res) => {
-    res.send('OK');
+    res.json(selectedTitle(result));
 });
 
 app.listen(3000, R.pipe(R.always('Listening to port 3000.'), console.log));
